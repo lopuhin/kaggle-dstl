e76043ef5677b33e7416d1d6e85335ee1452fbfd
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2017-01-26 11:22:26 +0300

    Fix submission merging: use sample_submission order

--- merge_submissions.py
+++ merge_submissions.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 import csv
+import gzip
 from pathlib import Path
 
 import utils  # for field_size_limit
@@ -12,21 +13,26 @@ def main():
     arg('root')
     args = parser.parse_args()
     root = Path(args.root)
-    out_path = root.joinpath('submission.csv')
-    header = None
-    all_data = []
+    out_path = root.joinpath('submission.csv.gz')
+    all_data = {}
     for path in root.glob('*.csv'):
-        if path != out_path:
-            print('Reading {}'.format(path))
-            with path.open() as f:
-                reader = csv.reader(f)
-                header = next(reader)
-                all_data.extend(reader)
-    all_data.sort(key=lambda x: (x[0], int(x[1])))
-    with out_path.open('wt') as f:
-        writer = csv.writer(f)
-        writer.writerow(header)
-        writer.writerows(all_data)
+        print('Reading {}'.format(path))
+        with path.open() as f:
+            reader = csv.reader(f)
+            next(reader)
+            all_data.update(
+                ((im_id, poly_type), poly) for im_id, poly_type, poly in reader)
+
+    with gzip.open(str(out_path), 'wt') as outf:
+        writer = csv.writer(outf)
+        with open('sample_submission.csv') as f:
+            reader = csv.reader(f)
+            header = next(reader)
+            writer.writerow(header)
+            for im_id, poly_type, _ in reader:
+                poly = (all_data.get((im_id, poly_type))
+                        or 'MULTIPOLYGON EMPTY')
+                writer.writerow([im_id, poly_type, poly])
 
 
 if __name__ == '__main__':
