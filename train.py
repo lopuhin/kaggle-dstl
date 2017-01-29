c90b49813c08ca51b2882a428e3c420f44167eb4
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2017-01-29 23:08:46 +0300

    Move models to a separate module

--- train.py
+++ train.py
@@ -19,149 +19,14 @@ from torch.autograd import Variable
 import torch.cuda
 import torch.optim as optim
 import torch.nn as nn
-import torch.nn.functional as F
 import tqdm
 
 import utils
+from models import HyperParams
+import models
 
 
 logger = utils.get_logger(__name__)
-
-
-@attr.s(slots=True)
-class HyperParams:
-    classes = attr.ib(default=list(range(10)))
-    net = attr.ib(default='DefaultNet')
-    n_channels = attr.ib(default=20)
-    total_classes = 10
-    thresholds = attr.ib(default=[0.2, 0.3, 0.4, 0.5, 0.6])
-
-    patch_inner = attr.ib(default=64)
-    patch_border = attr.ib(default=16)
-
-    dropout_keep_prob = attr.ib(default=0.0)  # TODO
-    jaccard_loss = attr.ib(default=0)
-
-    n_epochs = attr.ib(default=30)
-    oversample = attr.ib(default=0.0)
-    lr = attr.ib(default=0.0001)
-    batch_size = attr.ib(default=128)
-
-    @property
-    def n_classes(self):
-        return len(self.classes)
-
-    def update(self, hps_string: str):
-        if hps_string:
-            for pair in hps_string.split(','):
-                k, v = pair.split('=')
-                if k == 'classes':
-                    v = [int(x) for x in v.split('-')]
-                elif '.' in v:
-                    v = float(v)
-                elif k != 'net':
-                    v = int(v)
-                setattr(self, k, v)
-
-
-class BaseNet(nn.Module):
-    def __init__(self, hps: HyperParams):
-        super().__init__()
-        self.hps = hps
-        self.register_buffer('global_step', torch.IntTensor(1).zero_())
-
-
-class MiniNet(BaseNet):
-    def __init__(self, hps):
-        super().__init__(hps)
-        self.conv1 = nn.Conv2d(hps.n_channels, 4, 1)
-        self.conv2 = nn.Conv2d(4, 8, 3, padding=1)
-        self.conv3 = nn.Conv2d(8, hps.n_classes, 3, padding=1)
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = self.conv3(x)
-        b = self.hps.patch_border
-        return F.sigmoid(x[:, :, b:-b, b:-b])
-
-
-class OldNet(BaseNet):
-    def __init__(self, hps):
-        super().__init__(hps)
-        self.conv1 = nn.Conv2d(hps.n_channels, 64, 5, padding=2)
-        self.conv2 = nn.Conv2d(64, 64, 5, padding=2)
-        self.conv3 = nn.Conv2d(64, 64, 5, padding=2)
-        self.conv4 = nn.Conv2d(64, hps.n_classes, 7, padding=3)
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = self.conv4(x)
-        b = self.hps.patch_border
-        return F.sigmoid(x[:, :, b:-b, b:-b])
-
-
-class SmallNet(BaseNet):
-    def __init__(self, hps):
-        super().__init__(hps)
-        self.conv1 = nn.Conv2d(hps.n_channels, 64, 3, padding=1)
-        self.conv2 = nn.Conv2d(64, 64, 3, padding=1)
-        self.conv3 = nn.Conv2d(64, 64, 3, padding=1)
-        self.conv4 = nn.Conv2d(64, 128, 3, padding=1)
-        self.conv5 = nn.Conv2d(128, hps.n_classes, 3, padding=1)
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = F.relu(self.conv4(x))
-        x = self.conv5(x)
-        b = self.hps.patch_border
-        return F.sigmoid(x[:, :, b:-b, b:-b])
-
-
-# UNet:
-# http://lmb.informatik.uni-freiburg.de/people/ronneber/u-net/u-net-architecture.png
-
-
-class SmallUNet(BaseNet):
-    def __init__(self, hps):
-        super().__init__(hps)
-        self.conv1 = nn.Conv2d(hps.n_channels, 32, 3, padding=1)
-        self.conv2 = nn.Conv2d(32, 32, 3, padding=1)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
-        self.conv4 = nn.Conv2d(64, 64, 3, padding=1)
-        self.conv5 = nn.Conv2d(64, 32, 3, padding=1)
-        self.conv6 = nn.Conv2d(64, 32, 3, padding=1)
-        self.conv7 = nn.Conv2d(32, hps.n_classes, 3, padding=1)
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x1 = self.pool(x)
-        x1 = F.relu(self.conv3(x1))
-        x1 = F.relu(self.conv4(x1))
-        x1 = F.relu(self.conv5(x1))
-        x1 = upsample2d(x1)
-        x = torch.cat([x, x1], 1)
-        x = F.relu(self.conv6(x))
-        x = self.conv7(x)
-        b = self.hps.patch_border
-        return F.sigmoid(x[:, :, b:-b, b:-b])
-
-
-def upsample2d(x):
-    # repeat is missing: https://github.com/pytorch/pytorch/issues/440
-    # return x.repeat(1, 1, 2, 2)
-    x = torch.stack([x[:, :, i // 2, :] for i in range(x.size()[2] * 2)], 2)
-    x = torch.stack([x[:, :, :, i // 2] for i in range(x.size()[3] * 2)], 3)
-    return x
-
-
-DefaultNet = OldNet
 
 
 @attr.s
@@ -179,7 +44,7 @@ class Image:
 class Model:
     def __init__(self, hps: HyperParams):
         self.hps = hps
-        self.net = globals()[hps.net](hps)
+        self.net = getattr(models, hps.net)(hps)
         self.bce_loss = nn.BCELoss()
         self.optimizer = optim.Adam(self.net.parameters(), lr=hps.lr)
         self.tb_logger = None  # type: tensorboard_logger.Logger
