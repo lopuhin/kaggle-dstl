821124cdc115074484291ea707896de28f9a70ef
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2017-02-02 00:10:59 +0300

    more flexible UNet definition

--- models.py
+++ models.py
@@ -24,6 +24,8 @@ class HyperParams:
     dropout_keep_prob = attr.ib(default=0.0)  # TODO
     dice_loss = attr.ib(default=0)
 
+    filters_base = attr.ib(default=32)
+
     n_epochs = attr.ib(default=30)
     oversample = attr.ib(default=0.0)
     lr = attr.ib(default=0.0001)
@@ -147,63 +149,36 @@ class UNet(BaseNet):
     def __init__(self, hps):
         super().__init__(hps)
         self.pool = nn.MaxPool2d(2, 2)
-        conv = lambda c_in, c_out: nn.Conv2d(c_in, c_out, 3, padding=1)
-        self.conv1_1 = conv(hps.n_channels, 32)
-        self.conv1_2 = conv(32, 32)
-        self.conv2_1 = conv(32, 64)
-        self.conv2_2 = conv(64, 64)
-        self.conv3_1 = conv(64, 128)
-        self.conv3_2 = conv(128, 128)
-        self.conv4_1 = conv(128, 256)
-        self.conv4_2 = conv(256, 256)
-        self.conv5_1 = conv(256, 512)
-        self.conv5_2 = conv(512, 512)
-        self.conv6_1 = conv(512 + 256, 256)
-        self.conv6_2 = conv(256, 256)
-        self.conv7_1 = conv(256 + 128, 128)
-        self.conv7_2 = conv(128, 128)
-        self.conv8_1 = conv(128 + 64, 64)
-        self.conv8_2 = conv(64, 64)
-        self.conv9_1 = conv(64 + 32, 32)
-        self.conv9_2 = conv(32, 32)
-        self.conv10 = nn.Conv2d(32, hps.n_classes, 1)
+        conv3 = lambda in_, out: nn.Conv2d(in_, out, 3, padding=1)
+        sattr = lambda k, v: setattr(self, k, v)
+        b = hps.filters_base
+        self.filters = [b, b * 2, b * 4, b * 8, b * 16]
+        for i, nf in enumerate(self.filters):
+            low_nf = hps.n_channels if i == 0 else self.filters[i - 1]
+            # TODO - maybe make it a module?
+            sattr('conv_down_{}_1'.format(i), conv3(low_nf, nf))
+            sattr('conv_down_{}_2'.format(i), conv3(nf, nf))
+            if i != 0:
+                sattr('conv_up_{}_1'.format(i), conv3(low_nf + nf, low_nf))
+                sattr('conv_up_{}_2'.format(i), conv3(low_nf, low_nf))
+        self.conv_final = nn.Conv2d(self.filters[0], hps.n_classes, 1)
 
     def forward(self, x):
-        x1 = F.relu(self.conv1_1(x))
-        x1 = F.relu(self.conv1_2(x1))
+        xs = []
+        n = len(self.filters)
+        for i in range(n):
+            x_out = self.pool(xs[-1]) if xs else x
+            x_out = F.relu(getattr(self, 'conv_down_{}_1'.format(i))(x_out))
+            x_out = F.relu(getattr(self, 'conv_down_{}_2'.format(i))(x_out))
+            xs.append(x_out)
 
-        x2 = self.pool(x1)
-        x2 = F.relu(self.conv2_1(x2))
-        x2 = F.relu(self.conv2_2(x2))
+        x_out = xs[-1]
+        for i in range(n - 1, 0, -1):
+            x_skip = xs[i - 1]
+            x_out = torch.cat([upsample2d(x_out), x_skip], 1)
+            x_out = F.relu(getattr(self, 'conv_up_{}_1'.format(i))(x_out))
+            x_out = F.relu(getattr(self, 'conv_up_{}_2'.format(i))(x_out))
 
-        x3 = self.pool(x2)
-        x3 = F.relu(self.conv3_1(x3))
-        x3 = F.relu(self.conv3_2(x3))
-
-        x4 = self.pool(x3)
-        x4 = F.relu(self.conv4_1(x4))
-        x4 = F.relu(self.conv4_2(x4))
-
-        x5 = self.pool(x4)
-        x5 = F.relu(self.conv5_1(x5))
-        x5 = F.relu(self.conv5_2(x5))
-
-        x6 = torch.cat([upsample2d(x5), x4], 1)
-        x6 = F.relu(self.conv6_1(x6))
-        x6 = F.relu(self.conv6_2(x6))
-
-        x7 = torch.cat([upsample2d(x6), x3], 1)
-        x7 = F.relu(self.conv7_1(x7))
-        x7 = F.relu(self.conv7_2(x7))
-
-        x8 = torch.cat([upsample2d(x7), x2], 1)
-        x8 = F.relu(self.conv8_1(x8))
-        x8 = F.relu(self.conv8_2(x8))
-
-        x9 = torch.cat([upsample2d(x8), x1], 1)
-        x9 = F.relu(self.conv9_1(x9))
-        x9 = F.relu(self.conv9_2(x9))
-
-        x10 = self.conv10(x9)
+        x_out = self.conv_final(x_out)
         b = self.hps.patch_border
-        return F.sigmoid(x10[:, :, b:-b, b:-b])
+        return F.sigmoid(x_out[:, :, b:-b, b:-b])
