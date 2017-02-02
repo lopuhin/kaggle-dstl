00e7df1823356637af8d16e75dc803728e2cbfdc
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2017-02-03 00:42:08 +0300

    Always use logdir as a store

--- make_submission.py
+++ make_submission.py
@@ -45,9 +45,7 @@ def main():
         header = next(reader)
         image_ids = [im_id for im_id, cls, _ in reader if cls == '1']
 
-    assert args.output.endswith('.csv')
-    store = Path(args.output.split('.csv')[0])
-    store.mkdir(exist_ok=True)
+    store = args.logdir  # type: Path
 
     train_ids = set(utils.get_wkt_data())
     to_predict = set(train_ids)
