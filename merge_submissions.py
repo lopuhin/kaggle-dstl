ca884e7a4458933aadfd68d82ab3c566ad36678b
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2017-02-05 00:04:41 +0300

    An option to include only specific classes in merge_submissions

--- merge_submissions.py
+++ merge_submissions.py
@@ -2,7 +2,6 @@
 import argparse
 import csv
 import gzip
-from pathlib import Path
 
 import utils  # for field_size_limit
 
@@ -12,13 +11,15 @@ def main():
     arg = parser.add_argument
     arg('output')
     arg('inputs', nargs='+')
-    arg('--skip', nargs='+')
+    arg('--skip', nargs='+', help='{im_id}_{poly_type} format, e.g 6100_1_1_10')
+    arg('--cls', type=int, nargs='+', help='leave other classes empty')
     args = parser.parse_args()
     skip = set(args.skip or [])
     all_data = {}
-    for path in map(Path, args.inputs):
+    for path in args.inputs:
         print('Reading {}'.format(path))
-        with path.open() as f:
+        opener = gzip.open if path.endswith('.gz') else open
+        with opener(path, 'rt') as f:
             reader = csv.reader(f)
             next(reader)
             all_data.update(
@@ -32,7 +33,9 @@ def main():
             writer.writerow(header)
             for im_id, poly_type, _ in reader:
                 poly = 'MULTIPOLYGON EMPTY'
-                if '{}_{}'.format(im_id, poly_type) not in skip:
+                cls = int(poly_type) - 1
+                if ('{}_{}'.format(im_id, poly_type) not in skip
+                        and (not args.cls or cls in args.cls)):
                     poly = all_data.get((im_id, poly_type)) or poly
                 writer.writerow([im_id, poly_type, poly])
 
