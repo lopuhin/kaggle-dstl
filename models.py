b7ff3e3c9a03f91099f01d4b625a3788d3363981
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2017-02-13 01:41:30 +0300

    Optional area by cls scaling (awful)

--- models.py
+++ models.py
@@ -14,7 +14,7 @@ class HyperParams:
     net = attr.ib(default='UNet')
     n_channels = attr.ib(default=12)  # max 20
     total_classes = 10
-    thresholds = attr.ib(default=[0.4, 0.5, 0.6])
+    thresholds = attr.ib(default=[0.3, 0.4, 0.5, 0.6])
 
     patch_inner = attr.ib(default=64)
     patch_border = attr.ib(default=16)
@@ -29,6 +29,7 @@ class HyperParams:
     activation = attr.ib(default='relu')
     dice_loss = attr.ib(default=0.0)
     dist_loss = attr.ib(default=0.0)
+    area_weight_loss = attr.ib(default=0)
 
     filters_base = attr.ib(default=32)
 
