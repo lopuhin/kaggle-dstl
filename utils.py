00a80c6c16598133ccc32efb225beea32043fd2b
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2017-02-22 17:50:30 +0300

    align should be True by default

--- utils.py
+++ utils.py
@@ -44,7 +44,7 @@ def get_wkt_data() -> Dict[str, Dict[int, str]]:
     return _wkt_data
 
 
-def load_image(im_id: str, rgb_only=False, align=False) -> np.ndarray:
+def load_image(im_id: str, rgb_only=False, align=True) -> np.ndarray:
     im_rgb = tiff.imread('./three_band/{}.tif'.format(im_id)).transpose([1, 2, 0])
     if rgb_only:
         return im_rgb
